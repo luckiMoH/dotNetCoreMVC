c753f77cae532e263e50b53eb149c0182a540c4b
Mateusz Majewski <mat.maj1992@gmail.com>
2023-11-24 18:08:54 +0100

    Fluent Validation.

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -8,19 +8,14 @@ using System.Threading.Tasks;
 namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDto
-    {
-        [Required(ErrorMessage = "Please use string longer than 2 characters and less than 20")]
-        [StringLength(20, MinimumLength = 2)]
-        public string Name { get; set; } = default!;
-        [Required(ErrorMessage = "Please insert description")]
+    {   
+        public string Name { get; set; } = default!;   
         public string? Description { get; set; }
         public string? About { get; set; }
-        [StringLength(12, MinimumLength = 8)]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
         public string? PostalCode { get; set; }
-
         public string? EncodedName { get; set; }
     }
 }
---
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using CarWorkshop.Domain.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Application.CarWorkshop
+{
+    public class CarWorkshopDtoValidator : AbstractValidator<CarWorkshopDto>
+    {
+        public CarWorkshopDtoValidator(ICarWorkshopRepository repository)
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Imię jest wymagane.")
+                .MinimumLength(2).WithMessage("Imię powinno mięc minimum 2 znaki.")
+                .MaximumLength(20).WithMessage("Imię powinnie mieć maksymalnie 20 znaków.")
+                .Custom((value, context) =>
+                {
+                    var existingCarWorkshop = repository.GetByName(value).Result;
+                    if (existingCarWorkshop != null)
+                    {
+                        context.AddFailure($"{value} nie jest unikalną nazwą dla warsztatu");
+                    }
+                });
+
+            RuleFor(c => c.Description)
+                .NotEmpty().WithMessage("Podaj opis.");
+
+            RuleFor(c => c.PhoneNumber)
+                .MinimumLength(8)
+                .MaximumLength(12);
+        }
+    }
+}
